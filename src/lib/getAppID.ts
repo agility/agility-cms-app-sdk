349632e633d0bf416de0c3dd40b03f2c736f753c
Landy Simpson <simpslandyy@gmail.com>
2023-04-25 01:44:07 -0400

    Fix the getAppID

--- src/lib/getAppID.ts
+++ src/lib/getAppID.ts
@@ -1,5 +1,5 @@
 export const getAppID = () => {
-	if (typeof window === 'undefined') return ""
+	if (typeof window === 'undefined') return "-1"
 	const params = window.location.search
 	const urlParams = new URLSearchParams(params)
 	const id = urlParams.get('appID')
