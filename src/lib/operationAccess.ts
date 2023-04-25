7dd940da12c24bc96eb0b2beae99285e88949099
Landy Simpson <simpslandyy@gmail.com>
2023-04-25 01:43:55 -0400

    add a function to remove all operations for clean up and flag to prevent auto delete of operation

--- src/lib/operationAccess.ts
+++ src/lib/operationAccess.ts
@@ -3,10 +3,14 @@ import { Subject } from "rxjs"
 export interface IOperation<T> {
 	operationID: string
 	operation: Subject<T>
+	autoDelete?: boolean
 }
 
+interface IOperationsHash {
+ [operationID: string]: { operation: Subject<any>, autoDelete?: boolean } 
+}
 
-const operations: { [operationID: string]: Subject<any> } = {}
+const operations: IOperationsHash = {}
 
 /**
  * Adds an operation to the list of operations, indexed by the operationID
@@ -15,15 +19,27 @@ const operations: { [operationID: string]: Subject<any> } = {}
  * @param {IOperation<T>} { operationID, operation }
  * @returns
  */
-export const addOperation = <T>({ operationID, operation }: IOperation<T>) => {
-	operations[operationID] = operation
+export const addOperation = <T>({ operationID, operation, autoDelete = true }: IOperation<T>) => {
+	operations[operationID] = { operation, autoDelete }
 	return operation
 
 }
 
 export const getOperation = (operationID:string): Subject<any> | null => {
-	const op = operations[operationID]
-	delete operations[operationID]
-	return op || null
+	const { operation, autoDelete }: { operation: Subject<any>, autoDelete?: boolean } = operations[operationID]
 
+	if (autoDelete) {
+		delete operations[operationID]
+	}
+
+	return operation || null
+}
+
+
+export const removeOperations = () => {
+	for (const id of Object.getOwnPropertyNames(operations)) {
+		const { operation } = operations[id]
+		operation.unsubscribe()
+		delete operations[id]
+	}
 }
