f1f65e1b13642e2c1e0368f735e115f26b730495
Joel Varty <joel@agilitycms.com>
2023-06-01 11:24:40 -0400

    tweaks to props

--- src/hooks/useResizeHeight.tsx
+++ src/hooks/useResizeHeight.tsx
@@ -1,12 +1,17 @@
 import {  useEffect, RefObject } from "react"
 import { setHeight } from "../methods"
 
+interface Props {
+  ref: RefObject<HTMLElement>
+  padding?: number
+}
+
 /**
  * useResizeHeight detects the height of the current ref object, and triggers the setHeight method to update the Content Manager
  * @param ref - html ref.
  * @param padding - optional additional padding for the height
  */
-const useResizeHeight = (ref: RefObject<HTMLElement>, padding: number = 0) => {
+const useResizeHeight = ({ ref, padding = 0 }: Props) => {
   useEffect(() => {
     const mdeSizeElm = ref?.current
     if (!mdeSizeElm) return
