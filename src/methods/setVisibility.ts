cf36c5b79e8d04e452a5fb4d2aed9a0b999655fe
Joel Varty <joel@agilitycms.com>
2024-02-06 14:24:56 -0500

    comments

--- src/methods/setVisibility.ts
+++ src/methods/setVisibility.ts
@@ -6,9 +6,9 @@ import { addOperation } from "../lib/operationAccess"
 import { IAppEventParam, IAppVisibility } from "../types"
 
 /**
- * setVisibility
- * sends a message out to the iframe channel with a numeric height
- * @param visibility - true wills how the iframe, false will hdie the iframe
+ * Sets a field's visibility.
+ * @param param0
+ * @returns
  */
 export const setVisibility = ({ fieldName, visibility }: IAppVisibility) => {
 
